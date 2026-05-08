f8e08676bf2d35f484bc7c07274f64d5f897f7a6
lyandraalmeidas <lyandraalmeidadasilva99@gmail.com>
2026-05-08 13:23:08 -0300

    ajuste das cores e adiçao da logo no componente menu.jsx

--- lya-assets/src/components/Menu/Menu.jsx
+++ lya-assets/src/components/Menu/Menu.jsx
@@ -1,3 +1,4 @@
+import Logo from '../../assets/icons/logo.svg';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -8,7 +9,16 @@ function Menu () {
   return (
     <Navbar>
       <Container>
-        <Navbar.Brand href="#home">OnlySprite</Navbar.Brand>
+        <Navbar.Brand href="#home">
+           <img
+              alt=""
+              src={Logo}
+              width="50"
+              height="50"
+              className="d-inline-block align-center"
+            />{' '}
+           OnlySprite
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
