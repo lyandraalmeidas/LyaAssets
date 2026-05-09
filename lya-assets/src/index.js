091da4e477cfcf94e41620427c49db2241d30bab
lyandraalmeidas <lyandraalmeidadasilva99@gmail.com>
2026-05-09 20:33:15 -0300

    ajustes

--- lya-assets/src/index.js
+++ lya-assets/src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// No seu main.jsx
 import { RouterProvider } from "react-router-dom";
-import router from './Router.jsx';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
+import router from "./router"; // o arquivo que você criou
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
-)
+);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
