82970227e77ce55129052d0473428d2027931715
lyandraalmeidas <lyandraalmeidadasilva99@gmail.com>
2026-05-09 20:39:08 -0300

    removendo o background

--- lya-assets/src/App.js
+++ lya-assets/src/App.js
@@ -1,14 +1,12 @@
 import './App.css';
 import Menu from './components/Menu/Menu';
 import Footer from './components/Footer/Footer';
-import BackgroundStars from './components/BackgroundStars/BackgroundStars';
 import "bootstrap/dist/css/bootstrap.min.css";
 import {Outlet} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <BackgroundStars />
       <Menu />
       <Outlet />
       <Footer />
